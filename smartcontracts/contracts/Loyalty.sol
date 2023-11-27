e2dfb3a667142edb85772d59d743b8847ba9fa6d
Medet Ahmetson <ahmetson@zoho.com>
2023-11-27 07:02:37 +0700

    Add management of the shops

--- smartcontracts/contracts/Loyalty.sol
+++ smartcontracts/contracts/Loyalty.sol
@@ -21,6 +21,11 @@ contract Loyalty {
     event AnnounceLoyaltyPoints(address indexed shop, address indexed user, bytes32 receiptId, uint points, string dataFormat);
     event SubmitPersonalData(address shop, bytes32 receiptId, string userData);
 
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     modifier onlyShop() {
         require(shops[msg.sender], "not_shop");
         _;
@@ -28,6 +33,20 @@ contract Loyalty {
 
     constructor() {
         owner = msg.sender;
+    }
+
+    // Todo later we will add a fee mechanism for the Loyalty Service.
+    function addShop(address shop) external onlyOwner {
+        shops[shop] = true;
+    }
+
+    function removeShop(address shop) external onlyOwner {
+        delete shops[shop];
+    }
+
+    // Shops can remove themselves.
+    function removeShop() external onlyShop {
+        delete shops[msg.sender];
     }
 
     // The Shop announces a new exchange for the user data.
