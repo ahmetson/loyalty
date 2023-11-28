442b9c5e5e9d211d86427ae3e967f1a441142e2a
Medet Ahmetson <milayter@gmail.com>
2023-11-28 09:17:12 +0700

    Link the Hex to Loyalty in the deployment

--- smartcontracts/scripts/deploy.ts
+++ smartcontracts/scripts/deploy.ts
@@ -1,11 +1,24 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 async function main() {
-  const loyalty = await ethers.deployContract("Loyalty", []);
+  const hexLib = await ethers.deployContract("Hex", { });
+  console.log(`Waiting for a confirmation of hex deployment`);
 
-  console.log(
-    `Loyalty deployed to ${loyalty.address}`
-  );
+  let libRes = await hexLib.waitForDeployment();
+
+  let libAddr = await libRes.getAddress();
+  console.log(`Hex library deployed to ${libAddr}`);
+
+  const loyalty = await ethers.deployContract("Loyalty", [], {libraries: {"Hex": libAddr}});
+  console.log('Waiting for a confirmation of Loyalty deployment');
+
+  await loyalty.waitForDeployment();
+  let loyaltyAddr = await loyalty.getAddress();
+  console.log(`Loyalty deployed to ${loyaltyAddr}`);
+  console.log(`What's next?`);
+  console.log(`\t1. Verify the contract calling 'npx hardhat verify --network ${network.name} ${loyaltyAddr}'`);
+  console.log(`\t2. Set the JS function calling 'npx hardhat verify --network ${network.name} scripts/set_js_in_loyalty.ts'`);
+  console.log(`\t3. Add the smartcontract as the consumer in the Chainlink function: https://functions.chain.link/sepolia/1705`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
